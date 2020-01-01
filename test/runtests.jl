fcbe0af8586b6d18f6ff3aa9b01cf0c2da6b0d4f
Konrad Heidler <k.heidler@tum.de>
2020-01-01 13:45:40 +0100

    More sensible tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,31 +1,44 @@
 using ProgressBars, Test, Printf
 
+# Initialize bar to get default width
+bar = ProgressBar(1:10)
+pre = "Testing Progress Bars. All progress bars should end "
+post = " here ▕"
+num_dots = bar.width - length(pre) - length(post)
+println(pre * repeat(".", num_dots) * post)
+
 # Basic Test
-for i in ProgressBar(1:10)
+for i in bar
 end
 @test true
 
 # Test alias
-for i in tqdm(1:10)
+for i in tqdm(1:1000)
+  sleep(0.001)
 end
 @test true
 
 # Test with description
-iter = ProgressBar(1:100)
+iter = ProgressBar(1:1000)
 for i in iter
   # ... Neural Network Training Code
-  loss = exp(-i)
+  sleep(0.001)
+  loss = exp(-i / 1000)
   set_description(iter, string(@sprintf("Loss: %.2f", loss)))
 end
 @test true
 
-iter = ProgressBar(1:100000)
+iter = ProgressBar(1:1000)
 tic = time_ns()
 for i in iter
   i = i * 2
 end
 toc = time_ns()
 @test (toc - tic) / 1e9 < 1
+
+for i in ProgressBar(1:100, total=-1)
+  sleep(0.01)
+end
 
 # Test Threads for Julia 1.3
 if VERSION >= v"1.3.0"
