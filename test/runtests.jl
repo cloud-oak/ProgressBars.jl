ea92e34066c2edb7633a3a7cbae407bbdfc64235
Konrad Heidler <k.heidler@tum.de>
2021-05-17 09:48:22 +0200

    Make testing script output more readable

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,12 +7,12 @@ post = " here ▕"
 num_dots = bar.width - length(pre) - length(post)
 println(pre * repeat(".", num_dots) * post)
 
-println("Basic pbar test")
+println("> Basic pbar test")
 for i in bar
 end
 @test true
 
-println("Test time taken for a fast iteration (printing only every 50ms)")
+println("> Test time taken for a fast iteration (printing only every 50ms)")
 iter = ProgressBar(1:10000, printing_delay=0.05)
 tic = time_ns()
 for i in iter
@@ -20,9 +20,9 @@ for i in iter
 end
 toc = time_ns()
 @test (toc - tic) / 1e9 < 0.2
-println("Took $((toc - tic) * 1e-6)ms")
+println("> Took $((toc - tic) * 1e-6)ms")
 
-println("Test time taken when printing every iteration, should be much longer than previous one")
+println("> Test time taken when printing every iteration, should be much longer than previous one")
 iter = ProgressBar(1:10000, printing_delay=0)
 tic = time_ns()
 for i in iter
@@ -30,26 +30,26 @@ for i in iter
 end
 toc = time_ns()
 @test true
-println("Took $((toc - tic) * 1e-6)ms")
+println("> Took $((toc - tic) * 1e-6)ms")
 
-println("Test special case of single iteration progress bar")
+println("> Test special case of single iteration progress bar")
 for i in ProgressBar(1:1)
 end
 
-println("Test tqdm alias")
+println("> Test tqdm alias")
 for i in tqdm(1:1000)
   sleep(0.0001)
 end
 @test true
 
-println("Test print from within a ProgressBar Loop")
+println("> Test print from within a ProgressBar Loop")
 iter = ProgressBar(1:5)
 for i in iter
   println(iter, "Printing from iteration $i")
   sleep(0.2)
 end
 
-println("Test with description")
+println("> Test with description")
 iter = ProgressBar(1:1000)
 for i in iter
   # ... Neural Network Training Code
@@ -59,7 +59,7 @@ for i in iter
 end
 @test true
 
-println("Test with regular postfix")
+println("> Test with regular postfix")
 iter = ProgressBar(1:1000)
 for i in iter
   sleep(0.0001)
@@ -68,7 +68,7 @@ for i in iter
 end
 @test true
 
-println("Test with multiline postfix")
+println("> Test with multiline postfix")
 iter = ProgressBar(1:1000)
 for i in iter
   sleep(0.0001)
@@ -77,7 +77,7 @@ for i in iter
 end
 @test true
 
-println("Test with regular postfix and multiline postfix")
+println("> Test with regular postfix and multiline postfix")
 iter = ProgressBar(1:1000)
 for i in iter
   sleep(0.0001)
@@ -87,20 +87,20 @@ for i in iter
 end
 @test true
 
-println("Test with leave=false, there should be no pbar left below this!")
+println("> Test with leave=false, there should be no pbar left below this!")
 iter = ProgressBar(1:1000, leave=false)
 for i in iter
   sleep(0.0001)
 end
 @test true
 
-println("Testing pbar without total number specified")
+println("> Testing pbar without total number specified")
 for i in ProgressBar(1:100, total=-1)
   sleep(0.001)
 end
 @test true
 
-println("Testing Threads for Julia 1.3")
+println("> Testing Threads for Julia 1.3")
 if VERSION >= v"1.3.0"
   a = []
   Threads.@threads for i in ProgressBar(1:1000)
@@ -109,7 +109,7 @@ if VERSION >= v"1.3.0"
 end
 @test true
 
-println("Testing pbar with custom units")
+println("> Testing pbar with custom units")
 for i in ProgressBar(1:100, unit="flobberwobbles")
   sleep(0.001)
 end
